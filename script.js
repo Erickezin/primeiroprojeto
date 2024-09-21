757868f950d05f069c2d379863db38e14eb03b3f
ericke <erickerodolfoc@gmail.com>
2024-09-21 14:34:54 -0300

    imagem

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 function toggleMode() {
     const html = document.documentElement
     html.classList.toggle("light")
-
-
+  
+   
     const img = document.querySelector("#profile img")
+  
 
-    // substituir a imagem
     if (html.classList.contains("light")) {
      
-        img.setAttribute("src", "./assets/avatar-light.png")
+      img.setAttribute("src", "./assets/assets/avatar-light.png")
     } else {
-
-        img.setAttribute("src", "./assets/avatar.png")
+   
+      img.setAttribute("src", "./assets/assets/avatar.png")
     }
-}
+  }
